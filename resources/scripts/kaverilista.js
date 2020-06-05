b993d2d20136deca0299084a769e8b99a2595402
Pattuu <62293749+Pattuu@users.noreply.github.com>
2020-06-05 13:30:40 +0300

    Add files via upload

--- resources/scripts/kaverilista.js
+++ resources/scripts/kaverilista.js
@@ -27,5 +27,50 @@ document.getElementById('kl-0-clear').addEventListener("click", function() {
 
 
 
+var friends = [];
+
+document.getElementById('kl-1-submit').addEventListener('click', function() {
+    var inputAdd = document.getElementById('kl-1-input').value;
+    if (inputAdd === '') {
+        return false;
+    } else {
+        friends.push(inputAdd);
+        document.getElementById('kl-1-input').value = '';
+        document.getElementById('kl-1-lista').innerHTML = friends.join(' ');
+    };
+});
+
+document.getElementById('kl-1-poista').addEventListener('click', function() {
+    var inputDel = document.getElementById('kl-1-input').value;
+    if (inputDel === '') {
+        return false;
+    } else if (friends.includes(inputDel)) {
+        var index = friends.indexOf(inputDel);
+        friends.splice(index, 1);
+        document.getElementById('kl-1-input').value = '';
+        document.getElementById('kl-1-lista').innerHTML = friends.join(' ');
+    };
+});
+
+
+
+let sort = 0;
+document.getElementById('kl-1-järjestä').addEventListener('click', function() {
+    if (sort === 0) {
+        friends.sort();
+        sort++;
+        document.getElementById('kl-1-lista'). innerHTML = friends.join(' ');
+    } else if (sort === 1) {
+        friends.reverse();
+        sort = 0;
+        document.getElementById('kl-1-lista'). innerHTML = friends.join(' ');
+    };
+});
+
+document.getElementById('kl-1-clear').addEventListener('click', function() {
+    friends = [];
+    sort = 0;
+    document.getElementById('kl-1-lista').innerHTML = friends.join(' ');
+});
 
 
